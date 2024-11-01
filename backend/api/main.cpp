96e3456679aedc1a5ce044b4e47a3988c5cbd81e
Daniel Smit <dcsmit7@gmail.com>
2024-11-01 22:46:33 +1030

    Support CORS and return online status

--- backend/api/main.cpp
+++ backend/api/main.cpp
@@ -1,10 +1,27 @@
 #include "crow.h"
+#include "crow/middlewares/cors.h"
 
 int main() {
-    crow::SimpleApp app;
+  crow::App<crow::CORSHandler> app;
 
-    CROW_ROUTE(app, "/")([](){ return "Hello world"; });
-    CROW_ROUTE(app, "/connected")([](){ return true; });
+  auto& cors = app.get_middleware<crow::CORSHandler>();
 
-    app.port(18080).multithreaded().run();
+  cors.global()
+      .origin("*")
+      .allow_credentials()
+      .headers("Accept", "Origin", "Content-Type", "Authorization", "Refresh")
+      .methods(crow::HTTPMethod::GET, crow::HTTPMethod::POST,
+               crow::HTTPMethod::OPTIONS, crow::HTTPMethod::HEAD,
+               crow::HTTPMethod::PUT, crow::HTTPMethod::DELETE);
+
+  CROW_ROUTE(app, "/")([]() { return "Hello world"; });
+
+  CROW_ROUTE(app, "/state") ([]() {
+    crow::response res;
+    res.set_header("Content-Type", "application/json");
+    res.write(R"({"online": true})");
+    return res;
+  });
+
+  app.port(18080).multithreaded().run();
 }
